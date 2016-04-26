733e3619d36fb9845b63919b4e5e88b9b03d0c27
radim <monsoon78@volny.cz>
2016-04-26 13:58:03 +0200

    initial commit

---
+++ page01/page.js
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+
+//
+//    document.body.appendChild(document.createTextNode('Hello World!'));
+//
+//    var h1 = document.getElementById('header'); // holds a reference to the <h1> tag
+//    h1 = document.getElementsByTagName('h1')[0]; // accessing the same <h1> element
+
+function doSomething(){
+	alert("Hello");
+}
+
+function doSomethingOther(){
+	doSomething();
+}
+
+function calc(numA, numB)
+{
+	return numA + numB;
+}
